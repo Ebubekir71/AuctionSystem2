c268d91a85b809cb1a88461a4c04e5ed94362e91
Ebubekir Tosun <tosunbekir91@gmail.com>
2025-04-29 13:08:03 +0200

    Object create

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        Auction auction = new Auction();
+
+        AuctionTimer timer = new AuctionTimer();
+
+        Bidder bidder = new Bidder();
+
+        RobotBidder robotBidder = new RobotBidder();
+
+        BidListener bidListener = new Bidder();
 
 
 
